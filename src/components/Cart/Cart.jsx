86008f6efe66ea58ba6290b7813d1d16a5d682c1
Ornella Sereno Alloa <90015172+Ornella07@users.noreply.github.com>
2023-10-04 11:11:55 -0300

    Update Cart.jsx

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -7,9 +7,7 @@ import './Cart.css'
 
 const Cart = () => {
       const {cart, clearCart, totalQuantity, total} = useContext(CartContext)
-
       if(totalQuantity === 0){
-        
         return (
           <div className="carrito-vacio">
             <h1>No hay productos en el carrito</h1>
@@ -17,63 +15,17 @@ const Cart = () => {
           </div>
         )
       }
-     
-    
       return (
         <div>
           { cart.map((item => <CartItem key={item.id} {...item}/> ))}
           <div className="container">
-            <h3 className="total">Total: ${total}</h3>
               <button onClick={() => clearCart() } className='Button' > Limpiar Carrito </button>
-              <Link to='/Checkout'>Finalizar Compra</Link>
-              </div>
+              <Link to='/Checkout' className="Button">Finalizar Compra</Link>
+              <h3 className="total">Total: ${total}</h3>
+          </div>
               
         </div>
       )
 }
-
 export default Cart
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const {cart,addItem,removeItem, clearCart, isInCart } = useContext(
-//   CartContext
-// );
-
-// if (addItem === 0) {
-//   return (
-//     <div className="carrito">
-//       <h2>No hay productos en el carrito</h2>
-//       <Link to="/" className="btn btn-primary"> Ver productos </Link>
-//     </div>
-//   );
-// }
-
-// return (
-//   <div className="carrito">
-//     {cart.map((producto) => (
-//       <CartItem key={producto.id} {...producto} />
-//     ))}
-//     {/* <h3>Total: $ {total}</h3> */}
-//     {/* <h3>Cantidad total: {cantidadTotal}</h3> */}
-//     <button className="btn btn-danger" onClick={() => clearCart()}>
-//       Vaciar Carrito
-//     </button>
-//     <hr />
-//     <Link to="/checkout" className="btn btn-primary botonFinal">
-//       Finaliza Tu Compra
-//     </Link>
-//   </div>
-// );
-// };
