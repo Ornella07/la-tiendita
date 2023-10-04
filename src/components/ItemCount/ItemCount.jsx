300b117b6065d15b691f295d088ae8f7f278b5d0
Ornella Sereno Alloa <90015172+Ornella07@users.noreply.github.com>
2023-10-04 12:40:51 -0300

    Update ItemCount.jsx

--- src/components/ItemCount/ItemCount.jsx
+++ src/components/ItemCount/ItemCount.jsx
@@ -3,8 +3,6 @@ import "./ItemCount.css";
 import { Stack, Box,  Button } from "@chakra-ui/react";
 import React from "react";
 
-
-
 const ItemCount = ({stock, initial, addItem}) => {
 
     const [quantity, setQuantity] = useState( initial )
@@ -29,17 +27,16 @@ const ItemCount = ({stock, initial, addItem}) => {
                         </button>
                     </Box>
                     <Box w='50px' h='40px'>
-                       <h4 className="button-2">{quantity}</h4>
+                        <h4 className="button-2">{quantity}</h4>
                     </Box>
                     <Box  w='50px' h='40px'>
                     <button  className="button-2" onClick={increment}>
                             +
-                        </button>
-                    
+                    </button>
                     </Box>
                     <Box >
                     <Button className='boton-1' variant='dark' onClick={() => addItem(quantity)}>
-                        <span >Agregar al Carrito</span>
+                        <span>Agregar al Carrito</span>
                     </Button>
                     </Box>
                 </Stack>
