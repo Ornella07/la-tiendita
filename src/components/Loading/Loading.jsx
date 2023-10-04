e1912ad51b9b396039d0482ec14c11574ff52343
Ornella Sereno Alloa <90015172+Ornella07@users.noreply.github.com>
2023-10-04 11:18:46 -0300

    Update Loading.jsx

--- src/components/Loading/Loading.jsx
+++ src/components/Loading/Loading.jsx
@@ -1,18 +1,20 @@
 import React from "react";
 import { Spinner } from "@chakra-ui/react";
-
+import './Loading.css'
 const Loading = () => {
     return(
         <div className="Spinner">
             <Spinner
-            thickness='4px'
-            speed='0.65s'
-            // emptyColor='gray.200'
+            thickness='3px'
+            speed='0.60s'
             bgGradient="linear(to-l, rgb(90, 86, 86), #DF9169)"
             size='xl'
-            m='180px'
+            m='350px'
             w='200px'
             h='200px'
+            alignItems='center'
+            textAlign='center'
+            alignContent='center'
             />
         </div>
     )
